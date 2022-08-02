4baacb1a559ae0bf7510639f895796263234cc3d
RITIKA <RITIKA@DESKTOP-76LCFQA>
2022-08-02 16:52:20 +0530

    first commit

---
+++ Day 1_Git and GitHub/src/com/capgemini/github/EclipseWithGithub.java
@@ -0,0 +1,9 @@
+package com.capgemini.github;
+
+public class EclipseWithGithub {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to capgemini C2TC program");
+	}
+
+}
