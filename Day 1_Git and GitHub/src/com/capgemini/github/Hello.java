74aad85ea4bb1c38820f4b089c1d806919dae8fa
RITIKA <RITIKA@DESKTOP-76LCFQA>
2022-08-02 15:43:33 +0530

    second commit

--- Day 1_Git and GitHub/src/com/capgemini/github/Hello.java
+++ Day 1_Git and GitHub/src/com/capgemini/github/Hello.java
@@ -1,9 +1,11 @@
 package com.capgemini.github;
-
+//prgrm for byte data-tupe
 public class Hello {
 
 	public static void main(String[] args) {
-       System.out.println("Hello World");
+		byte a=127;
+		//byte a=129 error because of byte range
+       System.out.println(a);
 	}
 
 }
