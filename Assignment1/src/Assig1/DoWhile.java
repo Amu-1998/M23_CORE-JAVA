b113e4ac4971678bc67e98050bd137eb6777e167
RITIKA <RITIKA@DESKTOP-76LCFQA>
2022-08-07 18:29:35 +0530

    First commit

---
+++ Assignment1/src/Assig1/DoWhile.java
@@ -0,0 +1,16 @@
+package Assig1;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int i=1;
+		do
+		{
+			System.out.println(i+ " ahmed");
+			i++;
+		}
+		while(i<10);
+
+	}
+
+}
---
+++ Assignment1/src/Assig1/EnhancedForLoop.java
@@ -0,0 +1,13 @@
+package Assig1;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		int[] arrayy= {1,2,3,4,5};
+		for(int a:arrayy)
+		{
+			System.out.println(a);
+		}
+	}
+
+}
