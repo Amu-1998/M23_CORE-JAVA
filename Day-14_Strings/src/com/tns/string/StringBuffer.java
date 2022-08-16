5c6e57281a328f03a5a232e4526767151c53d80a
RITIKA <RITIKA@DESKTOP-76LCFQA>
2022-08-16 13:26:06 +0530

    Second commit

---
+++ Day-14_Strings/src/com/tns/string/StringBuffer.java
@@ -0,0 +1,18 @@
+package com.tns.string;
+
+public class StringBuffer {
+	public StringBuffer(String string) {
+		
+	}
+
+	public static void main(String[] args) {
+		StringBuffer s=new StringBuffer("How are you?");
+		s.reverse();
+		System.out.println(s);
+
+	}
+	private void reverse()  {
+		
+	}
+
+}
