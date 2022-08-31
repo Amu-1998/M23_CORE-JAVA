b06e562e4c7aa12a7e34945b73b34664e2022b6a
RITIKA <RITIKA@DESKTOP-76LCFQA>
2022-08-31 15:00:53 +0530

    First commit

---
+++ Day-22_Day Date and Time/src/com/cg/day_date_time/Example1.java
@@ -0,0 +1,15 @@
+package com.cg.day_date_time;
+
+import java.time.LocalDate;
+
+
+public class Example1 {
+	public static void main(String[] args) {
+
+		LocalDate date1=LocalDate.now();
+		LocalDate date2=date1.plusDays(12);
+		System.out.println(date2);
+				
+	}
+
+}
